5acafeea0307c884886018d94b1c17ad07d15b8b
Jorge Calleja <posicionadoenlaweb@gmail.com>
2025-05-05 18:48:37 +0200

    arbitrum sepolia config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,23 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env.local" });
+
+const privateKey = process.env.PRIVATE_KEY || "";
+// Asegúrate de que la clave privada tenga el formato correcto
+const formattedKey = privateKey.startsWith("0x")
+  ? privateKey
+  : `0x${privateKey}`;
+
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.27",
+  networks: {
+    hardhat: {},
+    arbitrumSepolia: {
+      url: "https://sepolia-rollup.arbitrum.io/rpc",
+      chainId: 421614, // ID de cadena para Arbitrum Sepolia
+      accounts: [formattedKey],
+    },
+  },
+  sourcify: {
+    enabled: true,
+  },
 };
